85b2ad597b13daf314cc1b1405e55a7ae5670051
Mountzionug <84102972+Mountzionug@users.noreply.github.com>
2021-10-18 13:09:54 +0530

    Update music-list.js

--- music-list.js
+++ music-list.js
@@ -2,60 +2,60 @@ let allMusic = [
   {
     name: "Second Chance",
     artist: "Apostle SsajalyaYesu",
-    img: "music-1",
+    img: "music-4",
     src: "music-1"
   },
   {
     name: "The Power of The House Of Jacob",
     artist: "Pastor Kitunzi",
-    img: "music-2",
+    img: "music-4",
     src: "music-2"
   },
   {
     name: "Don't be Afraid",
     artist: "Pastor Deo",
-    img: "music-3",
+    img: "music-4",
     src: "music-3"
   },
   {
     name: "Expalnation of 'I was glad whem they told me, Lets go into the house of the Lord'",
     artist: "Pastor Deo",
-    img: "music-4",
+    img: "music-3",
     src: "music-4"
   },
   {
     name: "Fear or Faith",
     artist: "Pastor Deo",
-    img: "music-5",
+    img: "music-4",
     src: "music-5"
   },
   {
     name: "Fear Or Faith( Part-2)",
     artist: "Pastor Deo",
-    img: "music-6",
+    img: "music-4",
     src: "music-6"
   },
   {
     name: "Prophecy Over Yourself)",
     artist: "Pastor Deo",
-    img: "music-6",
+    img: "music-1",
     src: "music-7"
   },
   {
     name: "Raise Your Faith)",
     artist: "Pastor Deo",
-    img: "music-6",
+    img: "music-4",
     src: "music-8"
   },{
     name: "The Lord God, Your God is with You",
     artist: "Pastor Deo",
-    img: "music-6",
+    img: "music-1",
     src: "music-9"
   },
   {
     name: "The only wisdom you can ever need",
     artist: "Pastor Deo",
-    img: "music-6",
+    img: "music-4",
     src: "music-10"
   },
   // like this paste it and remember to give comma after ending of this bracket }
